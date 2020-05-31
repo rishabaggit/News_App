131271772fccebba4f8bfe593f119bba03e3146d
Pranjal Tripathi <pranjal.tripathi07@gmail.com>
2020-05-31 18:00:28 +0530

    Fixed Header Colors

--- news_fk/src/components/Header/Header.js
+++ news_fk/src/components/Header/Header.js
@@ -6,10 +6,10 @@ const Header = (props) => {
     return(
 
             <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-primary fixed-top" >
+                <nav className="navbar navbar-expand-lg navbar-light fixed-top" >
                     <div className="container">
                     <Link className="navbar-brand" to="/"><i className="fa fa-globe" aria-hidden="true"></i> fLipkart News App</Link>
-                    <Link className="navbar-brand" to="/info"><i className="fa fa-globe" aria-hidden="true"></i> Info</Link>
+                    <Link className="nav-link" to="/info">Info</Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
