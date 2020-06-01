e1d5956dc01e8cf371a3fb85aab7b406d35c4239
harshavardhanaS <harshavardhana.s@flipkart.com>
2020-06-01 16:35:03 +0530

    Set up the basic structure for redux

--- news_fk/src/App.js
+++ news_fk/src/App.js
@@ -10,86 +10,118 @@ import {API_PAGE_SIZE} from './constants';
 import {BrowserRouter,Route} from 'react-router-dom';
 import NewsBulletin from './components/Content/NewsBulletin/NewsBulletin';
 import Info from './components/Info/Info';
+import {connect} from 'react-redux';
+import * as actionTypes from './store/actions';
+
 class App extends Component {
-  state = {
-    initial_loading : true,
-    should_load : false,
-    currpage : 1,
-    countrycode : "in",
-    newscategory : "General",
-    newsarticles : [],
-    end_of_article : false,
-    error_found : false
-  }
+  // state = {
+  //   initial_loading : true,
+  //   should_load : false,
+  //   currpage : 1,
+  //   countrycode : "in",
+  //   newscategory : "General",
+  //   newsarticles : [],
+  //   end_of_article : false,
+  //   error_found : false
+  // }
   newsHandler = () => {
-    if(!this.state.end_of_article) {
-      this.setState({should_load : false})
+    if(!this.props.end_of_article) {
+      // this.setState({should_load : false});
+      this.props.setShouldLoad(false);
       // const url = `http://newsapi.org/v2/top-headlines?country=${this.state.countrycode}&category=${this.state.newscategory}&pageSize=${API_PAGE_SIZE}&page=${this.state.currpage}&apiKey=e004421173114bd5b890eb56590a9a12`;
-      const url = `http://newsapi.org/v2/top-headlines?country=${this.state.countrycode}&category=${this.state.newscategory}&pageSize=${API_PAGE_SIZE}&page=${this.state.currpage}&apiKey=8115e82c195f445ebba7f0beeeb524e2`;
+      const url = `http://newsapi.org/v2/top-headlines?country=${this.props.countrycode}&category=${this.props.newscategory}&pageSize=${API_PAGE_SIZE}&page=${this.props.currpage}&apiKey=8115e82c195f445ebba7f0beeeb524e2`;
       fetch(url)
       .then(response => {
         return response.json();
       })
       .then(fin => {
-        this.setState({
-          initial_loading : false,
-          newsarticles : [...this.state.newsarticles , ...fin.articles],
-          should_load : true
-        })
+        this.props.setInitialLoading(false);
+        this.props.setNewsArticles([...this.props.newsarticles , ...fin.articles]);
+        this.props.setShouldLoad(true);
+        // this.setState({
+        //   initial_loading : false,
+        //   newsarticles : [...this.props.newsarticles , ...fin.articles],
+        //   should_load : true
+        // })
         if(fin.articles.length < API_PAGE_SIZE) {
-          this.setState({end_of_article : true})
+          this.props.setEndOfArticle(true);
+          //this.setState({end_of_article : true})
           console.log("triggered");
         }
       })
-      .catch(err => {this.setState({error_found : true})})
+      .catch(err => {
+        // this.setState({error_found : true})
+        this.props.setErrorFound(true);
+      })
   
-      this.setState((prevState,props) => {
-        return {
-          currpage : prevState.currpage+1
-        }
-      });
+      // this.setState((prevState,props) => {
+      //   return {
+      //     currpage : prevState.currpage+1
+      //   }
+      // });
+      this.props.setCurrPage(this.props.currpage + 1);
     }
   }
   countryChangeHandler = (event) => {
-    this.setState({
-      initial_loading : true,
-      countrycode : event.target.value,
-      currpage : 1,
-      newsarticles : [],
-      should_load : false,
-      end_of_article : false,
-      error_found : false
-    }
-    , this.newsHandler);
+    // this.setState({
+    //   initial_loading : true,
+    //   countrycode : event.target.value,
+    //   currpage : 1,
+    //   newsarticles : [],
+    //   should_load : false,
+    //   end_of_article : false,
+    //   error_found : false
+    // }
+    // , this.newsHandler);
+    this.props.setInitialLoading(true);
+    this.props.setCountryCode(event.target.value);
+    this.props.setCurrPage(1);
+    this.props.setNewsArticles([]);
+    this.props.setShouldLoad(false);
+    this.props.setEndOfArticle(false);
+    this.props.setErrorFound(false);
+
+    this.newsHandler();
   }
   categoryChangeHandler = (event) => {
-    this.setState({
-      initial_loading : true,
-      newscategory : event.target.value,
-      currpage : 1,
-      newsarticles : [],
-      should_load : false,
-      end_of_article : false,
-      error_found : false
-    }, this.newsHandler);
+    // this.setState({
+    //   initial_loading : true,
+    //   newscategory : event.target.value,
+    //   currpage : 1,
+    //   newsarticles : [],
+    //   should_load : false,
+    //   end_of_article : false,
+    //   error_found : false
+    // }, this.newsHandler);
+    this.props.setInitialLoading(true);
+    this.props.setNewsCategory(event.target.value);
+    this.props.setCurrPage(1);
+    this.props.setNewsArticles([]);
+    this.props.setShouldLoad(false);
+    this.props.setEndOfArticle(false);
+    this.props.setErrorFound(false);
+
+    this.newsHandler();
   }
+
   onBottomHandler = () => {
-    if(this.state.should_load) {
+    if(this.props.should_load) {
       this.newsHandler();
     }
   }
+  
   Display = () => {
-    if(this.state.error_found) {
+    if(this.props.error_found) {
       return <FetchErrorHandler/>;
     }
-    else if(this.state.initial_loading) {
+    else if(this.props.initial_loading) {
       return <InitialLoader/>;
     }
     else {
       return (
         <div>
-            <DisplayNewsCards articles={this.state.newsarticles}/>
-            <BottomLoader load={!this.state.end_of_article} />
+            <DisplayNewsCards articles={this.props.newsarticles}/>
+            <BottomLoader load={!this.props.end_of_article} />
         </div>
       );
     }
@@ -100,8 +132,8 @@ class App extends Component {
         <div>
           <BottomScrollListener onBottom={this.onBottomHandler}/>
           <Header 
-            country={this.state.countrycode}
-            category={this.state.newscategory}
+            country={this.props.countrycode}
+            category={this.props.newscategory}
             countryChangeHandler={this.countryChangeHandler}
             categoryChangeHandler={this.categoryChangeHandler}
           />
@@ -117,4 +149,32 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    initial_loading : state.initial_loading,
+    should_load : state.should_load,
+    currpage : state.currpage,
+    countrycode : state.countrycode,
+    newscategory : state.newscategory,
+    newsarticles : state.newsarticles,
+    end_of_article : state.end_of_article,
+    error_found : state.error_found
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+      setInitialLoading : (newVal) => dispatch({type : actionTypes.SET_INITIAL_LOADING, val : newVal}),
+      setShouldLoad : (newVal) => dispatch({type : actionTypes.SET_SHOULD_LOAD, val : newVal}),
+      setCurrPage : (newVal) => dispatch({type : actionTypes.SET_CURR_PAGE, val : newVal}),
+      setCountryCode : (newVal) => dispatch({type : actionTypes.SET_COUNTRY_CODE, val : newVal}),
+      setNewsCategory : (newVal) => dispatch({type : actionTypes.SET_NEWS_CATEGORY, val : newVal}),
+      setNewsArticles : (newVal) => dispatch({type : actionTypes.SET_NEWS_ARTICLES, val : newVal}),
+      setEndOfArticle : (newVal) => dispatch({type : actionTypes.SET_END_OF_ARTICLE, val : newVal}),
+      setErrorFound : (newVal) => dispatch({type : actionTypes.SET_ERROR_FOUND, val : newVal})
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+// export default App;
--- news_fk/src/store/reducer.js
+++ news_fk/src/store/reducer.js
@@ -1,7 +1,60 @@
-const initialState = {
+import * as actionTypes from './actions';
 
+const initialState = {
+    initial_loading : true,
+    should_load : false,
+    currpage : 1,
+    countrycode : "in",
+    newscategory : "General",
+    newsarticles : [],
+    end_of_article : false,
+    error_found : false
 }
+
 const reducer = (state = initialState,action) => {
+    switch(action.type){
+        case actionTypes.SET_INITIAL_LOADING:
+            return{
+                ...state,
+                initial_loading : action.val
+            }
+        case actionTypes.SET_SHOULD_LOAD:
+            return{
+                ...state,
+                should_load : action.val
+            }
+        case actionTypes.SET_CURR_PAGE:
+            return{
+                ...state,
+                currpage : action.val
+            }    
+        case actionTypes.SET_COUNTRY_CODE:
+            return{
+                ...state,
+                countrycode : action.val
+            } 
+        case actionTypes.SET_NEWS_CATEGORY:
+            return{
+                ...state,
+                newscategory : action.val
+            } 
+        case actionTypes.SET_NEWS_ARTICLES:
+            return{
+                ...state,
+                newsarticles : action.val
+            }   
+        case actionTypes.SET_END_OF_ARTICLE:
+            return{
+                ...state,
+                end_of_article : action.val
+            }
+        case actionTypes.SET_ERROR_FOUND:
+            return{
+                ...state,
+                error_found : action.val
+            }
+    }
     return state;
 }
+
 export default reducer;
