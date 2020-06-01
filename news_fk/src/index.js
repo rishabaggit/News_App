3a92dcd90b2f9c3fce39dbf31e5f4130ffc9d67b
Mudreka Arif <mudrekaarif@gmail.com>
2020-06-01 13:47:51 +0530

    Introduced redux

--- news_fk/src/index.js
+++ news_fk/src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {createStore} from 'redux'
 import registerServiceWorker from './registerServiceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import reducer from './store/reducer';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer);
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
