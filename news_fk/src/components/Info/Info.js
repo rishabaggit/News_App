a93a381e5e1a9d82f3356a7c9b74783fb823bed9
Pranjal Tripathi <pranjal.tripathi07@gmail.com>
2020-06-02 15:50:03 +0530

    Resolved Team Page Window Flexibility

--- news_fk/src/components/Info/Info.js
+++ news_fk/src/components/Info/Info.js
@@ -9,15 +9,22 @@ const Info = (props) => {
     return (
         // <h1>INFO</h1
         <React.Fragment>
-<div className="container">
-    <div className='row'>
-        <div className ='rowContainer column'>
-            <Person name='Harshavardhana' imageSrc = {Pic1} title='SDE Intern at Flipkart'/>
-            <Person name='Mudreka Arif' imageSrc = {Pic2} title='SDE Intern at Flipkart'/>
-            <Person name='Pranjal Tripathi' imageSrc = {Pic3} title='SDE Intern at Flipkart'/>
+        {/* <div className="container">
+            <div className='row'>
+                <div className ='rowContainer column'>
+                    <Person name='Harshavardhana' imageSrc = {Pic1} title='SDE Intern at Flipkart'/>
+                    <Person name='Mudreka Arif' imageSrc = {Pic2} title='SDE Intern at Flipkart'/>
+                    <Person name='Pranjal Tripathi' imageSrc = {Pic3} title='SDE Intern at Flipkart'/>
+                </div>
+            </div>
+        </div> */}
+        <div className="container">
+            <div className="row">
+                <Person name='Harshavardhana' imageSrc = {Pic1} title='SDE Intern at Flipkart'/>
+                <Person name='Mudreka Arif' imageSrc = {Pic2} title='SDE Intern at Flipkart'/>
+                <Person name='Pranjal Tripathi' imageSrc = {Pic3} title='SDE Intern at Flipkart'/>
+            </div>
         </div>
-    </div>
-</div>
 
 </React.Fragment>
     );
--- news_fk/src/components/Info/Person.js
+++ news_fk/src/components/Info/Person.js
@@ -3,9 +3,9 @@ import './Info.css'
 
 const Person = (props) => {
     return (
-    <div className='column'>
+    <div className='column col-md-4 col-sm-6 col-xs-12'>
         <div className='card'>
-        <img className='image' src={props.imageSrc} alt=''/>
+        <img className='image'  src={props.imageSrc} alt=''/>
             <div className='container'>
                 <h2>{props.name}</h2>
                 <p className='title'>{props.title}</p>
