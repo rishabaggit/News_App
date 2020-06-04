8a10bd1c613ab5e94082bb66c77f503b64cbf45a
Mudreka Arif <mudrekaarif@gmail.com>
2020-06-04 22:42:37 +0530

    Modal shifted to a new component

--- news_fk/src/components/Content/DisplayNewsCards/NewsCard/NewsCard.js
+++ news_fk/src/components/Content/DisplayNewsCards/NewsCard/NewsCard.js
@@ -5,6 +5,7 @@
 
 import React from 'react';
 import "./NewsCard.css"
+import Modal from './Modal/Modal';
 
 //-----------------------------------------------------------------------------------------------------------------
 
@@ -27,53 +28,22 @@ const NewsCard = (props) => {
                             <p className="title">{props.title}</p>
                             <p className="maxLines">{props.description}...</p>
                             
-                            <button type="button" class="btn  mb-2" data-toggle="modal" data-target="#exampleModal"><span>Share</span></button>
-                            <button type="button" class="btn mb-2"  onClick={() => {
+                            <button type="button" 
+                                class="btn  mb-2" 
+                                data-toggle="modal" 
+                                data-target={'#' + props.url}>
+                                    <span>Share</span>
+                            </button>
+
+                            <button type="button" 
+                                class="btn mb-2"
+                                onClick={() => {
                                     var win = window.open(props.url, '_blank');
                                     win.focus();
                                 }}>
                                 <span>Read More</span>   
                             </button>
-
-                            {/* ---------------------------------MODAL BODY----------------------------------------- */}
-
-                            <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-hidden="true">
-                                <div className="modal-dialog modal-lg" role="document">
-                                    <div className="modal-content">
-                                    <div className="modal-header">
-                                        <h5 className="modal-title" style={{textAlign:'center'}} >{props.title}</h5>
-                                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <div className="container-fluid">
-                                            <div className="center">
-                                                <img alt='' style={{height:250, marginBottom:10}} src={props.img}/>
-                                            </div>
-
-                                            <div className="center">
-                                                <p>{props.description}</p>
-                                            </div>
-                                                <p>
-                                                    <a href="https://www.facebook.com" class="fa fa-facebook-square"></a>
-                                                    <a href="https://www.twitter.com" class="fa fa-twitter"></a>
-                                                    <a href="https://www.linkedin.com" class="fa fa-linkedin-square"></a>
-                                                    <a href="https://www.github.com" class="fa fa-github"></a>
-                                                    
-                                                </p>
-                                            
-                                        </div>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                        
-                                    </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            {/* ------------------------------------------------------------------------------------------ */}
+                            <Modal {...props}/>
                         </div>
                 </div>
                 
