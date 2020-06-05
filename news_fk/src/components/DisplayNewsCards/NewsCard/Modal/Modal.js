cb16edb48f869ba54df8df8a6ced2c53a54ab0cc
harshavardhanaS <harshavardhana.s@flipkart.com>
2020-06-05 21:21:52 +0530

    Enabled sharing of posts

--- news_fk/src/components/DisplayNewsCards/NewsCard/Modal/Modal.js
+++ news_fk/src/components/DisplayNewsCards/NewsCard/Modal/Modal.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { TwitterShareButton, TwitterIcon, FacebookShareButton, FacebookIcon, RedditShareButton, RedditIcon, WhatsappShareButton, WhatsappIcon} from 'react-share';
+
+
 
 const Modal = (props) => {
     return (
@@ -21,10 +24,38 @@ const Modal = (props) => {
                             <p>{props.description}</p>
                         </div>
                             <p>
-                                <a href="https://www.facebook.com" class="fa fa-facebook-square"></a>
+                            <FacebookShareButton
+                                url={props.url}>
+                                <FacebookIcon
+                                size={32}
+                                round />
+                            </FacebookShareButton>
+
+                            <TwitterShareButton
+                                url={props.url}>
+                                <TwitterIcon
+                                size={32}
+                                round />
+                            </TwitterShareButton>
+                            
+                            <RedditShareButton
+                                url={props.url}>
+                                <RedditIcon
+                                size={32}
+                                round />
+                            </RedditShareButton>
+
+                            <WhatsappShareButton
+                                url={props.url}>
+                                <WhatsappIcon
+                                size={32}
+                                round />
+                            </WhatsappShareButton>
+                            
+                                {/* <a href={`https://facebook.com/sharer/sharer.php?u=${encodeURI(props.url)}`} class="fa fa-facebook-square"></a>
                                 <a href="https://www.twitter.com" class="fa fa-twitter"></a>
                                 <a href="https://www.linkedin.com" class="fa fa-linkedin-square"></a>
-                                <a href="https://www.github.com" class="fa fa-github"></a>
+                                <a href="https://www.github.com" class="fa fa-github"></a> */}
                                 
                             </p>
                         
