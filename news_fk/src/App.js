66b9a1817736f8463a5eb9a2173699620f9ec0a2
harshavardhanaS <harshavardhana.s@flipkart.com>
2020-06-06 19:12:09 +0530

    Fixed background colour issues

--- news_fk/src/App.js
+++ news_fk/src/App.js
@@ -16,10 +16,22 @@ import {connect} from 'react-redux';
 //-----------------------------------------------------------------------------------------------------------------
 //index.js is the traditional and actual entry point for all node apps. Here in react it just has code of what to render and where to render. 
 // App.js has the root component of the react app because every view and component are handled with hierarchy in React, where <App /> is the top most component in hierarchy. 
-class App extends Component {  
+class App extends Component { 
+
+  componentDidMount(){
+    document.body.style.backgroundColor = this.props.colorsObj.backgroundColor;
+  };
+  componentDidUpdate(){
+    document.body.style.backgroundColor = this.props.colorsObj.backgroundColor;
+  };
+  
+  componentWillUnmount(){
+    document.body.style.backgroundColor = null;
+  }
+
   render() {
     return (                                     //Component able to render and read routes should be wrapped in BrowserRouter Object.
-      <div style = {{backgroundColor:this.props.colorsObj.backgroundColor}}>
+      <div>
           <Header/>
           <Route path="/" exact component={NewsBulletin}/>
           <Switch>
