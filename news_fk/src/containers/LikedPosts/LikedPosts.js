de118d54287f21df28d1def731ba9b1a4349a39b
Mudreka Arif <mudrekaarif@gmail.com>
2020-06-08 19:25:55 +0530

    Added LikedPosts page where All  likes of user are showed as news cards

---
+++ news_fk/src/containers/LikedPosts/LikedPosts.js
@@ -0,0 +1,112 @@
+// PURPOSE: Component is called in App.js and is responsible for Rendering of News Articles stored in newsarticles array
+
+//-----------------------------------------------------------------------------------------------------------------
+// Importing a named module or parameter to be used in DisplayNewsCards.js
+
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import NewsCardDB from '../../components/DisplayNewsCards/NewsCard/NewsCardDB';
+import {connect} from 'react-redux';
+import "../../components/DisplayNewsCards/DisplayNewsCards.css";
+import {userget , addLike , removeLike}from '../../components/UserData/FirestoreUtil';
+import FullScreenLoader from '../../components/UI/FullScreenLoader/FullScreenLoader';
+//-----------------------------------------------------------------------------------------------------------------
+class LikedPosts extends React.Component {
+    componentDidMount() {
+        if(this.props.userId === null) {
+            return <Redirect to="/signin" />;
+        }
+        else {
+            console.log('CALLED', this.props.userId)
+            userget(this.props.userId).then(
+                doc => {
+                    console.log('REACHED', doc)
+                    if(doc && doc.liked) {
+                        this.setState({LikedArray : doc.liked});
+                    }
+                    else {
+                        this.setState({LikedArray : []});
+                    }
+                }
+            )
+            .catch(error => console.log(error));
+        }
+    }
+    state = {
+        LikedArray : null
+    }
+    likePost = (newsItem) => {
+        let likedPosts = [...this.state.LikedArray , newsItem];
+        this.setState({LikedArray : likedPosts})
+        addLike(this.props.userId ,newsItem);
+    }
+    unlikePost = (newsItem) => {
+        let likedPosts = this.state.LikedArray;
+        const index = likedPosts.indexOf(newsItem);
+        if (index > -1) {
+            likedPosts.splice(index, 1);
+        }
+        this.setState({LikedArray : likedPosts})
+        removeLike(this.props.userId ,newsItem);
+    }
+    render() {
+        if(!this.props.userId) {
+            return <Redirect to="/signin" />;
+        }
+        if(this.state.LikedArray) {
+            return(
+                <div style={{marginTop : "50px"}}>
+                <div className="container">
+                    <div className="row">
+                            {
+                                 this.state.LikedArray.map((newsitem,idx) => {
+                                    return <NewsCardDB
+                                        img={newsitem.urlToImage}
+                                        url={newsitem.url}
+                                        title={newsitem.title}
+                                        description={newsitem.description}
+                                        key={idx}
+                                        colorsObj = {this.props.colorsObj}
+                                        Liked = {this.state.LikedArray.find(obj => obj.url === newsitem.url)}
+                                        newsItem={newsitem}
+                                        likePost = {this.likePost}
+                                        unlikePost = {this.unlikePost}
+                                    />
+                                })
+        
+                            }                
+                    </div>
+                </div>
+                </div>
+                
+            );
+            }
+            else {
+                return <FullScreenLoader/>;
+        }
+    }
+ }
+ 
+ //---------------------------------------------------------------------------------------------------------------
+//Exporting as default DisplayNewsCards
+
+const mapStateToProps = (state) => {
+    return {
+      userId : state.auth.userId,
+      colorsObj : state.appModeReducer.colorsObj
+    };
+  };
+
+//mapDispatchToProps() is a utility which will help your component to fire an action event
+
+// const mapDispatchToProps = dispatch => {
+//     return {
+//     }
+//   };
+  
+//-----------------------------------------------------------------------------------------------------------------
+//Using Default Export as App with Connect being an higher order component which provides data to Component and functions it can dispatch to store.
+
+export default connect(mapStateToProps, null)(LikedPosts);
+
+//---------------------------------------------------------------------------------------------------------------
