a5f42f51e177b2b02b02b7d8d00a377da2089032
Pranjal Tripathi <pranjal.tripathi07@gmail.com>
2020-06-12 18:29:12 +0530

    Completed cleaning all files

--- news_fk/src/containers/Auth/LogOut.tsx
+++ news_fk/src/containers/Auth/LogOut.tsx
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {logout} from '../../store/actions/index';
+import { logout } from '../../store/actions/index';
 import { Redirect } from 'react-router-dom';
 import FullScreenLoader from '../../components/UI/FullScreenLoader/FullScreenLoader'
 import { authAction } from 'store/actions/auth';
 import { RootState } from 'index';
 
 
-interface LogOutProps{
+interface LogOutProps {
     logout: () => authAction;
     userId: string;
-    cookies : any
+    cookies: any
 }
 
 class LogOut extends Component<LogOutProps> {
     componentDidMount() {
-        (this.props.cookies).set('PrevUser','',{path: '/'});
-        setTimeout(this.props.logout,500);
+        (this.props.cookies).set('PrevUser', '', { path: '/' });
+        setTimeout(this.props.logout, 500);
     }
     render() {
-        if(this.props.userId) {
-            return (<div><FullScreenLoader/></div>);
+        if (this.props.userId) {
+            return (<div><FullScreenLoader /></div>);
         }
         else {
             return <Redirect to="/" />;
@@ -30,16 +30,14 @@ class LogOut extends Component<LogOutProps> {
 
 const mapStateToProps = (state: RootState) => {
     return {
-      userId : state.auth.userId
+        userId: state.auth.userId
     };
-  };
-
-//mapDispatchToProps() is a utility which will help your component to fire an action event
+};
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        logout : () => dispatch(logout())
+        logout: () => dispatch(logout())
     }
-  };
-  
+};
+
 export default connect(mapStateToProps, mapDispatchToProps)(LogOut);
